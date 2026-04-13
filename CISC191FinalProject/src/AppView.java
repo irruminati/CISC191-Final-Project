490f4457483c6afba0fafcbfc4980b05edb3e362
irruminati <106195985+irruminati@users.noreply.github.com>
2026-04-13 11:13:04 -0700

    Project setup question mark

---
+++ CISC191FinalProject/src/AppView.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+/**
+ * Lead Author(s):
+ * 
+ * @author Rumi Chadwick
+ * @author Ale Hernandez
+ * 
+ *         References:
+ *         Morelli, R., & Walde, R. (2016).
+ *         Java, Java, Java: Object-Oriented Problem Solving
+ *         https://open.umn.edu/opentextbooks/textbooks/java-java-java-object-oriented-problem-solving
+ *
+ *         Version: 2026-04-13
+ */
+public class AppView extends JFrame
+{
+
+}
