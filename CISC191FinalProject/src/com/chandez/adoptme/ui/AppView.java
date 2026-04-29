0334a3eed291fa72b754946237a068601a21b824
alehzp <alehzp@10.214.29.183>
2026-04-29 11:54:58 -0700

    Started AppView class and SwipeView

---
+++ CISC191FinalProject/src/com/chandez/adoptme/ui/AppView.java
@@ -0,0 +1,25 @@
+package com.chandez.adoptme.ui;
+
+import java.awt.Dimension;
+import javax.swing.*;
+
+/**
+ * Lead Author(s):
+ * 
+ * @author Rumi Chadwick
+ * @author Ale Hernandez
+ * 
+ *         Version: 2026-04-13
+ */
+public class AppView extends JFrame
+{
+	private JPanel swipeView; // the cards
+
+	private JLabel ourNames; // bottom middle
+
+	public AppView()
+	{
+
+	}
+
+}
---
+++ CISC191FinalProject/src/com/chandez/adoptme/ui/SwipeView.java
@@ -0,0 +1,36 @@
+/**
+* Lead Author(s):
+* @author alehzp; student ID
+* @author Full name; student ID
+* <<Add additional lead authors here>>
+*
+* Other Contributors:
+* Full name; student ID or contact information if not in class
+* <<Add additional contributors (mentors, tutors, friends) here, with contact information>>
+*
+* References:
+* Morelli, R., & Walde, R. (2016).
+* Java, Java, Java: Object-Oriented Problem Solving
+* https://open.umn.edu/opentextbooks/textbooks/java-java-java-object-oriented-problem-solving
+*
+* <<Add more references here>>
+*
+* Version: 2026-04-29
+*/
+package com.chandez.adoptme.ui;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+/**
+ * Purpose: The reponsibility of SwipeView is ...
+ *
+ * SwipeView is-a ...
+ * SwipeView is ...
+ */
+public class SwipeView extends JPanel
+{
+	private JPanel petView;
+	private JButton yesButton;
+	private JButton noButton;
+}
