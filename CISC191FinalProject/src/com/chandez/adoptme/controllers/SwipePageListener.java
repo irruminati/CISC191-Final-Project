d5d63db2dbf590fcb7a8b1a75e30a94ed8bfee81
irruminati <106195985+irruminati@users.noreply.github.com>
2026-05-06 10:14:08 -0700

    Changed logic for swiping to next Pet

--- CISC191FinalProject/src/com/chandez/adoptme/controllers/SwipePageListener.java
+++ CISC191FinalProject/src/com/chandez/adoptme/controllers/SwipePageListener.java
@@ -3,11 +3,6 @@
  * 
  * @author Rumi Chadwick
  * @author Ale Hernandez
- * 
- *         References:
- *         Morelli, R., & Walde, R. (2016).
- *         Java, Java, Java: Object-Oriented Problem Solving
- *         https://open.umn.edu/opentextbooks/textbooks/java-java-java-object-oriented-problem-solving
  *
  *         Version: 2026-04-29
  */
--- CISC191FinalProject/src/com/chandez/adoptme/ui/SwipeView.java
+++ CISC191FinalProject/src/com/chandez/adoptme/ui/SwipeView.java
@@ -5,11 +5,9 @@ package com.chandez.adoptme.ui;
  * 
  * @author Rumi Chadwick
  * @author Ale Hernandez
- *
- *         References:
- *         Morelli, R., & Walde, R. (2016).
- *         Java, Java, Java: Object-Oriented Problem Solving
- *         https://open.umn.edu/opentextbooks/textbooks/java-java-java-object-oriented-problem-solving
+ * 
+ *         Other Contributors:
+ *         Alex Chow
  *
  *         Version: 2026-04-29
  */
